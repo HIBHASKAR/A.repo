7913212ffea2da72c8929d314dd192a8134008c8
bhask <bhask@LAPTOP-I1MU4LJ5.mshome.net>
2024-11-07 14:48:54 +0530

    try to do it on time

---
+++ A.framework/src/test/java/to_create_ORG/To_crate_org.java
@@ -0,0 +1,10 @@
+package to_create_ORG;
+
+public class To_crate_org {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ A.framework/src/test/java/to_create_contact/To_crate_contact.java
@@ -0,0 +1,10 @@
+package to_create_contact;
+
+public class To_crate_contact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
