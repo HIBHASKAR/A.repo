c0edaf19007b474f007fcc4af1d1366259ef0a5b
Adarsh <Adarsh@192.168.137.193>
2024-11-07 15:05:45 +0530

    code written in both script

--- A.framework/src/test/java/to_create_ORG/To_crate_org.java
+++ A.framework/src/test/java/to_create_ORG/To_crate_org.java
@@ -3,8 +3,8 @@ package to_create_ORG;
 public class To_crate_org {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println(9/3);
 	}
 
 }
--- A.framework/src/test/java/to_create_contact/To_crate_contact.java
+++ A.framework/src/test/java/to_create_contact/To_crate_contact.java
@@ -3,8 +3,8 @@ package to_create_contact;
 public class To_crate_contact {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println(7+3);
 	}
 
 }
